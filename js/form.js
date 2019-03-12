a72279636aebb58f7dd3dd352f4b886ea7d7d285
Chi-Quyen Le <quyen.lechi@gmail.com>
2019-03-11 21:04:50 -0700

    Update nav header based on login state

--- js/form.js
+++ js/form.js
@@ -1,4 +1,19 @@
 var submitButton = document.getElementById("submit");
-submitButton.addEventListener("click", function() {
+submitButton.addEventListener("click", function () {
     window.location.href = "../views/reference_sheet.html";
 });
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        // User is signed in.
+        $("#login").css("display", "none");
+        $("#logout").css("display", "inline");
+    } else {
+        // No user is signed in.
+        $("#login").css("display", "inline");
+        $("#logout").css("display", "none");
+    }
+});
+
