2f348f589e9af166c5c0db5f71297acfa48502ac
Edmond Wong <wongedmondk@gmail.com>
2019-03-14 11:31:51 -0700

    Added js for submit button to depend on user state

--- js/form.js
+++ js/form.js
@@ -1,7 +1,7 @@
 var submitButton = document.getElementById("submit");
 submitButton.addEventListener("click", function () {
-    window.location.href = "../views/reference_sheet.html";
-});
+  window.location.href = "../views/reference_sheet.html";
+})
 
 firebase.initializeApp(firebaseConfig);
 
@@ -10,10 +10,16 @@ firebase.auth().onAuthStateChanged(function (user) {
         // User is signed in.
         $("#login").css("display", "none");
         $("#logout").css("display", "inline");
+        submitButton.addEventListener("click", function () {
+          window.location.href = "../views/notificationWarning";
+        })
     } else {
         // No user is signed in.
         $("#login").css("display", "inline");
         $("#logout").css("display", "none");
+        submitButton.addEventListener("click", function () {
+          window.location.href = "../views/reference_sheet.html";
+        })
     }
 });
 
