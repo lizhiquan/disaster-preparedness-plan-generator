d980ccc83250fc5f2efa8216442fbd61ae4cda14
Chi-Quyen Le <quyen.lechi@gmail.com>
2019-03-14 12:06:36 -0700

    Rename with hyphen case

--- js/form.js
+++ js/form.js
@@ -1,7 +1,7 @@
 var submitButton = document.getElementById("submit");
 submitButton.addEventListener("click", function () {
     saveFormData();
-    window.location.href = "../views/reference_sheet.html";
+    window.location.href = "../views/reference-sheet.html";
 });
 
 firebase.initializeApp(firebaseConfig);
@@ -11,16 +11,10 @@ firebase.auth().onAuthStateChanged(function (user) {
         // User is signed in.
         $("#login").css("display", "none");
         $("#logout").css("display", "inline");
-        submitButton.addEventListener("click", function () {
-          window.location.href = "../views/notificationWarning";
-        })
     } else {
         // No user is signed in.
         $("#login").css("display", "inline");
         $("#logout").css("display", "none");
-        submitButton.addEventListener("click", function () {
-          window.location.href = "../views/reference_sheet.html";
-        })
     }
 });
 
