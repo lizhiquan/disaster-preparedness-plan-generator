8204570e81ef039abbc8e51705eb8bffde1d07c6
Chi-Quyen Le <quyen.lechi@gmail.com>
2019-03-14 12:01:30 -0700

    Get data from local storage for generating PDF

---
+++ js/reference-sheet.js
@@ -0,0 +1,10 @@
+function generatePDF() {
+    var storage = window.localStorage;
+
+    var fullname = storage.getItem("fullname");
+    var postalCode = storage.getItem("postalCode");
+    var familySize = storage.getItem("familySize");
+    var children = storage.getItem("children");
+    var medication = storage.getItem("medication");
+    var mobility = storage.getItem("mobility");
+}
