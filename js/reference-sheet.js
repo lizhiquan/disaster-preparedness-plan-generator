6ab237a6171157cf4e3b2e1bf20af7294a9b6547
Chi-Quyen Le <quyen.lechi@gmail.com>
2019-03-25 23:23:37 -0700

    Implement generating PDF feature from local storage

--- js/reference-sheet.js
+++ js/reference-sheet.js
@@ -1,22 +1,37 @@
 var buttonModify = document.getElementById("buttonModify");
 
-buttonModify.addEventListener("click", function() {
-  document.location.href = "../views/form.html";
+buttonModify.addEventListener("click", function () {
+    document.location.href = "../views/form.html";
 });
 
 var buttonHome = document.getElementById("buttonHome");
 
-buttonHome.addEventListener("click", function() {
-  document.location.href = "../index.html";
+buttonHome.addEventListener("click", function () {
+    document.location.href = "../index.html";
 })
 
-function generatePDF() {
+function getFormDataFromLocalStorage() {
     var storage = window.localStorage;
 
-    var fullname = storage.getItem("fullname");
-    var postalCode = storage.getItem("postalCode");
-    var familySize = storage.getItem("familySize");
-    var children = storage.getItem("children");
-    var medication = storage.getItem("medication");
-    var mobility = storage.getItem("mobility");
+    return {
+        fullname: storage.getItem("fullname"),
+        postalCode: storage.getItem("postalCode"),
+        familySize: storage.getItem("familySize"),
+        children: storage.getItem("children"),
+        medication: storage.getItem("medication"),
+        mobility: storage.getItem("mobility")
+    }
 }
+
+function generatePDF(formData) {
+    var doc = new jsPDF();
+    var row = 1;
+    for (let key in formData) {
+        doc.text(key + ": " + formData[key], 20, row * 10 + 10);
+        row++;
+    }
+    var pdfBlob = doc.output('blob', 'DPPG.pdf');
+    $('#tab-1').attr('src', URL.createObjectURL(pdfBlob));
+}
+
+generatePDF(getFormDataFromLocalStorage());
