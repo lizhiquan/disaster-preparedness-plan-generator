3cf5ac54f2c98e60f66c1345bbd266af93a759a4
Prateek Girdhar <prateek.sgirdhar@gmail.com>
2019-03-29 10:25:09 -0700

    Added delete button to settings rows

--- js/settings.js
+++ js/settings.js
@@ -94,12 +94,9 @@ function generateRowID() {
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     var length = 10;
-
     for (var i = 0; i < length; i++)
         text += possible.charAt(Math.floor(Math.random() * possible.length));
-
     return text;
-
 }
 
 function fetchSettingsData(userId) {
